2220047a0a0a51c6c9494d7d1d9b4a59717cc586
alain guillon <a.guillon@it-students.fr>
2023-01-25 09:01:00 +0100

    ajout de l'api

--- controllers/Api.js
+++ controllers/Api.js
@@ -1,7 +1,60 @@
+const TaskModel = require("../models/TaskModel");
+
 const getApi = (req, res) => {
-  res.send("api");
+  res.redirect("/api/tasks");
+};
+
+const getApiTasks = (req, res) => {
+  TaskModel.find()
+    .then((documents) => res.status(200).json(documents))
+    .catch((err) => console.log(err));
+};
+
+const getApiTaskRedirect = (req, res) => {
+  res.redirect("/api/tasks");
+};
+
+const getApiTaskID = (req, res) => {
+  TaskModel.findById(req.params.id)
+    .then((document) => {
+      // console.log(document)
+      res.status(200).json(document);
+    })
+    .catch((err) => console.log(err));
+};
+
+const getApiTaskDeleteID = (req, res) => {
+  TaskModel.deleteOne({ _id: req.params.id })
+    .exec()
+    .then(() => res.redirect("/api/tasks"))
+    .catch((err) => console.log(err));
+};
+
+const getApiTaskAdd = (req, res) => {
+  res.redirect("/api/task/add");
+};
+
+const postApiTaskAdd = (req, res) => {
+  const newTask = new TaskModel({
+    label: "Label de l'élément",
+    date: "2022-10-01",
+    description: "Description de l'élément",
+    status: false,
+  });
+  newTask
+    .save()
+    .then(() => res.redirect("/api/tasks"))
+    .catch((err) => console.log(err));
+  console.log("Data ajouté avec succès :\n\n");
+  console.log(newTask);
 };
 
 module.exports = {
   getApi,
+  getApiTasks,
+  getApiTaskRedirect,
+  getApiTaskID,
+  getApiTaskDeleteID,
+  getApiTaskAdd,
+  postApiTaskAdd,
 };
--- routes/taskAPI.routes.js
+++ routes/taskAPI.routes.js
@@ -2,5 +2,13 @@ const router = require("express").Router();
 const myController = require("../controllers/Api");
 
 router.route("/").get(myController.getApi);
+router.route("/tasks").get(myController.getApiTasks);
+router.route("/task").get(myController.getApiTaskRedirect);
+router.route("/task/:id").get(myController.getApiTaskID);
+router.route("/task/delete").get(myController.getApiTaskRedirect);
+router.route("/task/delete/:id").get(myController.getApiTaskDeleteID);
+router.route("/task/add").get(myController.getApiTaskAdd);
+
+router.route("/task/add").post(myController.postApiTaskAdd);
 
 module.exports = router;
